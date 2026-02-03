a8f0b1b9f837fc4022114d73b948fff7ed401606
liuweihong <liuweihong8@huawei.com>
2026-02-03 16:40:41 +0800

    解决门禁报错

--- samples/contribute/ACT/inc/model_process.h
+++ samples/contribute/ACT/inc/model_process.h
@@ -1,12 +1,19 @@
-/**
-* @file model_process.h
-*
-* Copyright (C) 2021. Shenshu Technologies Co., Ltd. All rights reserved.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-*/
+ /*
+ * Copyright (c) 2026 Huawei Technologies Co., Ltd
+ * This file is part of [Hispark/modelzoo].
+ *
+ * [Hispark/modelzoo] is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, version 3 of the License only.
+ *
+ * [Hispark/modelzoo] is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with [Hispark/modelzoo].  If not, see <https://www.gnu.org/licenses/>.
+ */
 #ifndef MODEL_PROCESS_H
 #define MODEL_PROCESS_H
 
@@ -18,108 +25,177 @@
 class ModelProcess {
 public:
     /**
-    * @brief Constructor
+    * Function: Class constructor
+    * Description: Initialize a ModelProcess instance with default resource states
     */
     ModelProcess();
 
     /**
-    * @brief Destructor
+    * Function: Class destructor
+    * Description: Release all allocated resources and destroy the ModelProcess instance
     */
     ~ModelProcess();
 
     /**
-    * @brief load model from file with mem
-    * @param [in] modelPath: model path
-    * @return result
+    * Function: Load model file into memory and initialize model resources
+    * Input: modelPath - full file path of the target model file (including file name and extension)
+    * Return: Result status code (success if the code indicates normal execution, failure otherwise)
     */
     Result LoadModelFromFileWithMem(const std::string& modelPath);
 
     /**
-    * @brief unload model
+    * Function: Unload loaded model and release associated memory
+    * Description: Clear model-related resources without returning any status
     */
     void Unload();
 
     /**
-    * @brief create dataset
-    * @return result
+    * Function: Initialize input dataset for model inference
+    * Return: Result status code (success/failure of initialization)
     */
     Result InitInput();
 
     /**
-    * @brief create model desc
-    * @return result
+    * Function: Create model description structure
+    * Description: Build and initialize the model descriptor to store model metadata
+    * Return: Result status code (success/failure of descriptor creation)
     */
     Result CreateDesc();
 
     /**
-    * @brief destroy desc
+    * Function: Destroy model description structure
+    * Description: Release memory occupied by the model descriptor and clear related data
     */
     void DestroyDesc();
 
     /**
-    * @brief create model input
-    * @param [in] inputDataBuffer: input buffer
-    * @param [in] bufferSize: input buffer size
-    * @return result
+    * Function: Create input buffer for model inference
+    * Input: inputDataBuffer - pointer to the host/device buffer containing input data
+    * Input: bufferSize - total byte size of the inputDataBuffer
+    * Input: stride - alignment step size for input data processing
+    * Return: Result status code (success/failure of input buffer creation)
     */
     Result CreateInput(void *inputDataBuffer, size_t bufferSize, int stride);
 
+    /**
+    * Function: Create input buffer from a specified file
+    * Input: filePath - full path of the file containing input data
+    * Return: Result status code (success/failure of buffer creation)
+    */
     Result CreateInputBuf(const std::string& filePath);
 
+    /**
+    * Function: Create task buffer and work buffer for model execution
+    * Return: Result status code (success/failure of buffer allocation)
+    */
     Result CreateTaskBufAndWorkBuf();
 
     /**
-    * @brief destroy input resource
+    * Function: Release all resources related to model input
+    * Description: Free input buffers and clear input dataset configuration
     */
     void DestroyInput();
 
     /**
-    * @brief create output buffer
-    * @return result
+    * Function: Allocate output buffer to store model inference results
+    * Return: Result status code (success/failure of output buffer creation)
     */
     Result CreateOutput();
 
     /**
-    * @brief destroy output resource
+    * Function: Release all resources related to model output
+    * Description: Free output buffers and clear inference result data
     */
     void DestroyOutput();
 
     /**
-    * @brief model execute
-    * @return result
+    * Function: Execute model inference with prepared input data
+    * Return: Result status code (success/failure of inference execution)
     */
     Result Execute();
 
     /**
-    * @brief dump model output result to file
+    * Function: Export model inference output results to a file
+    * Description: Write output buffer data to a local file (read-only operation, no state change)
     */
     void DumpModelOutputResult() const;
 
     /**
-    * @brief get model output result
+    * Function: Retrieve and display model inference output results
+    * Description: Extract data from output buffer and present it (read-only operation, no state change)
     */
     void OutputModelResult() const;
 
+    /**
+    * Function: Create dedicated buffer for specified input/output index
+    * Input: index - target index of the input/output buffer (starting from 0)
+    * Return: Result status code (success/failure of buffer creation)
+    */
     Result CreateBuf(int index);
 
     // Result GetInputStrideParam(int index, size_t& bufSize, size_t& stride, svp_acl_mdl_io_dims& dims) const;
 
+    /**
+    * Function: Get stride parameters of specified output index
+    * Input: index - target index of the output buffer (starting from 0)
+    * Output: bufSize - byte size of the output buffer (output parameter)
+    * Output: stride - alignment step size of the output data (output parameter)
+    * Output: dims - dimension information of the output model I/O (output parameter)
+    * Return: Result status code (success/failure of parameter retrieval)
+    */
     Result GetOutputStrideParam(int index, size_t& bufSize, size_t& stride, svp_acl_mdl_io_dims& dims) const;
 
+    /**
+    * Function: Get data size of specified input index
+    * Input: index - target index of the input buffer (starting from 0)
+    * Return: Total byte size of the input buffer at the specified index
+    */
     size_t GetInputDataSize(int index) const;
 
+    /**
+    * Function: Get data size of specified output index
+    * Input: index - target index of the output buffer (starting from 0)
+    * Return: Total byte size of the output buffer at the specified index
+    */
     size_t GetOutputDataSize(int index) const;
 
-    // 新增：获取模型输入个数
+    /**
+    * Function: Get the total number of model input nodes
+    * Return: Count of input nodes (non-negative integer)
+    */
     size_t GetInputNum() const;
-    // 新增：获取指定索引输入的参数（大小、stride、维度）
+
+    /**
+    * Function: Get stride parameters of specified input index
+    * Input: index - target index of the input buffer (starting from 0)
+    * Output: buf_size - byte size of the input buffer (output parameter)
+    * Output: stride - alignment step size of the input data (output parameter)
+    * Output: dims - dimension information of the input model I/O (output parameter)
+    * Return: Result status code (success/failure of parameter retrieval)
+    */
     Result GetInputStrideParam(int index, size_t& buf_size, size_t& stride, svp_acl_mdl_io_dims& dims) const;
 
+    /**
+    * Function: Create model input buffer from raw data
+    * Input: data - pointer to the raw input data buffer
+    * Input: data_size - total byte size of the raw data buffer
+    * Return: Result status code (success/failure of input buffer creation)
+    */
     Result CreateInputFromData(const void* data, size_t data_size); 
 
+    /**
+    * Function: Create model input buffers from multiple raw data sources
+    * Input: input_datas - vector of pointers to multiple raw input data buffers
+    * Input: input_sizes - vector of byte sizes corresponding to each input data buffer
+    * Return: Result status code (success/failure of multi-input buffer creation)
+    */
     Result CreateInputFromData(const std::vector<const void*>& input_datas, 
                                const std::vector<size_t>& input_sizes);
     
+    /**
+    * Function: Release all model-related resources
+    * Description: Unified release of input/output buffers, model descriptor, task/work buffers, etc.
+    */
     void DestroyResource();
 
 private:
--- samples/contribute/ACT/inc/sample_process.h
+++ samples/contribute/ACT/inc/sample_process.h
@@ -1,12 +1,20 @@
-/**
-* @file sample_process.h
-*
-* Copyright (C) 2021. Shenshu Technologies Co., Ltd. All rights reserved.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-*/
+ /*
+ * Copyright (c) 2026 Huawei Technologies Co., Ltd
+ * This file is part of [Hispark/modelzoo].
+ *
+ * [Hispark/modelzoo] is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, version 3 of the License only.
+ *
+ * [Hispark/modelzoo] is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with [Hispark/modelzoo].  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #ifndef SAMPLE_PROCESS_H
 #define SAMPLE_PROCESS_H
 
@@ -16,62 +24,89 @@
 #include "model_process.h"
 
 /**
-* SampleProcess
+* Class: SampleProcess
+* Description: Core processing class for model inference sample, manages input data, model loading and resource lifecycle
 */
 class SampleProcess {
 public:
     /**
-    * @brief Constructor
+    * Function: Class constructor
+    * Description: Initialize a SampleProcess instance with default values for all member variables
     */
     SampleProcess();
 
     /**
-    * @brief Destructor
+    * Function: Class destructor
+    * Description: Release allocated resources and reset member variable states when instance is destroyed
     */
     ~SampleProcess();
 
     /**
-    * @brief init reousce
-    * @return result
+    * Function: Initialize system and model inference related resources
+    * Description: Set up device context, stream and basic runtime environment
+    * Return: Result status code (success if the code indicates normal execution, failure otherwise)
     */
     Result InitResource();
 
     /**
-    * @brief sample process
-    * @return result
+    * Function: Execute the full process of sample inference
+    * Description: Orchestrate input data preparation, model loading and inference execution
+    * Return: Result status code (success/failure of the entire inference process)
     */
     Result Process();
     // void DestroyResource();
 
+    /**
+    * Function: Set the file path of the input data source
+    * Input: path - full file path of the input data file (including file name and extension)
+    */
     void SetInputPath(const std::string& path);
-    std::string input_path_;  // 用于存储输入文件路径
+    std::string input_path_;  // Member variable: Stores the full file path of the input data for inference
 
-    const char* input_data_ = nullptr;  // 二进制数据指针
-    size_t input_data_size_ = 0;        // 数据大小
+    const char* input_data_ = nullptr;  // Member variable: Pointer to the binary input data buffer (nullptr by default)
+    size_t input_data_size_ = 0;        // Member variable: Total byte size of the binary input data buffer (0 by default)
 
+    /**
+    * Function: Set multiple input data sources for multi-input model inference
+    * Input: input_datas - Vector of pointers to multiple binary input data buffers
+    * Input: input_sizes - Vector of byte sizes corresponding to each input data buffer
+    */
     void SetInputDatas(const std::vector<const void*>& input_datas, 
                        const std::vector<size_t>& input_sizes);
-    std::vector<const void*> input_datas_;
-    std::vector<size_t> input_sizes_;
+    std::vector<const void*> input_datas_;  // Member variable: Stores pointers to multiple input data buffers
+    std::vector<size_t> input_sizes_;       // Member variable: Stores byte sizes of corresponding multiple input data buffers
 
-    // 新增：声明 LoadModel 方法
+    /**
+    * Function: Load pre-configured model file into memory
+    * Description: Initialize model resources and prepare for inference execution
+    * Return: Result status code (success/failure of model loading)
+    */
     Result LoadModel();
+
+    /**
+    * Function: Release all allocated resources
+    * Description: Unified release of device context, stream, model resources and input/output buffers
+    */
     void DestroyResource();
 
 private:
-    // 新增：存储从Python传递的当前文件路径
-    int32_t deviceId_ { 0 };
-    svp_acl_rt_context context_ { nullptr };
-    svp_acl_rt_stream stream_ { nullptr };
+    int32_t deviceId_ { 0 };  // Member variable: ID of the target computing device (default to 0)
+    svp_acl_rt_context context_ { nullptr };  // Member variable: Runtime context of the computing device (nullptr by default)
+    svp_acl_rt_stream stream_ { nullptr };    // Member variable: Runtime stream for asynchronous execution (nullptr by default)
 
-    bool isInited_ = false;
-    ModelProcess modelProcess_;  // 改为成员变量，而非局部变量
-    bool isModelLoaded_ = false;
+    bool isInited_ = false;  // Member variable: Flag indicating if resource initialization is completed (false by default)
+    ModelProcess modelProcess_;  // Member variable: Instance of ModelProcess class (used for model management, not local variable)
+    bool isModelLoaded_ = false;  // Member variable: Flag indicating if model loading is completed (false by default)
 };
 
-// 新增友元函数（仅头文件，不修改cpp），用于传递路径（不新增类方法）
+/**
+* Function: Friend function to set input file path for SampleProcess instance
+* Description: Directly access the input_path_ member variable of SampleProcess (no need to add new Set method)
+* Input: sample - Reference to the target SampleProcess instance
+* Input: path - Full file path to be set for input data
+*/
 inline void set_input_path(SampleProcess& sample, const std::string& path) {
-    sample.input_path_ = path;  // 直接访问私有input_path_，无需新增Set方法
+    sample.input_path_ = path;  // Assign input file path to the member variable of the SampleProcess instance
 }
 
 #endif // SAMPLE_PROCESS_H
