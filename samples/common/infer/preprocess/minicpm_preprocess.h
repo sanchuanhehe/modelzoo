b710d8ef57f1e944f6615fdb208a053aa350a22d
筱逍 <18563517105@163.com>
2026-02-06 20:51:23 +0800

    add MiniCPM

--- samples/common/infer/preprocess/minicpm_preprocess.h
+++ samples/common/infer/preprocess/minicpm_preprocess.h
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) ModelZoo. 2026-2026. All rights reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #pragma once
 
 #include "dev_interface_adapter.h"
@@ -43,21 +58,6 @@ constexpr int32_t MAX_TOKEN_LEN = 1024;
 constexpr int32_t MINICPM_END_ID = 73440;
 constexpr int32_t TOTAL_TOKEN_NUM = 73448;
 constexpr int32_t ROTARY_POSITION_EMB_DATA_OFFSET = 64;
-/*
- * Copyright (c) ModelZoo. 2026-2026. All rights reserved.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 constexpr int32_t MAX_POSITION_IDX = 32768;
 const int32_t CONFIGS_NUM = 6;
 constexpr int32_t MASK_MIN_VALUE = -9999999;
